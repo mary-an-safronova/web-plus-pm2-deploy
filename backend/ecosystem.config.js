334d7015b0d49271224ff5c41abb547cc137c5e5
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-12-20 17:26:26 +0300

    Описаны сценарии деплоя pm2

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,29 @@
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/master',
+  DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'backend',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': `cd ${DEPLOY_PATH} && npm i && npm run build && pm2 restart all`,
+    },
+  },
+};
---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,30 @@
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/master',
+  DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps : [{
+    name   : 'frontend',
+    script : './build/index.html'
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': `cd ${DEPLOY_PATH} && npm i && npm run build`,
+    },
+  },
+}
+
