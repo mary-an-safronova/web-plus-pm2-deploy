6914df9004699d4a52d9879cf492a41ba734a9c6
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-12-20 13:31:51 +0300

    добален домен, который может совершать запрос к серверу

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
 // import cors from 'cors';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
@@ -15,6 +16,13 @@ mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
 // app.use(cors())
+app.use(cors({
+  origin: [
+    'http://mesto.mary.nomoredomainsmonster.ru',
+    'https://mesto.mary.nomoredomainsmonster.ru',
+  ],
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
