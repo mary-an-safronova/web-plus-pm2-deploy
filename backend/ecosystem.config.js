f81aff42b317af885725d580957a804f37b087bc
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-12-21 17:46:34 +0300

    исправлен ecosystem.config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,7 +2,6 @@ require('dotenv').config({ path: '.env.deploy' });
 
 const {
   DEPLOY_USER,
-  DEPLOY_KEY,
   DEPLOY_HOST,
   DEPLOY_PATH,
   DEPLOY_REF = 'origin/master',
@@ -19,13 +18,11 @@ module.exports = {
   deploy: {
     production: {
       user: DEPLOY_USER,
-      key: DEPLOY_KEY,
       host: DEPLOY_HOST,
-      ssh_options: 'StrictHostKeyChecking=no',
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
       'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
     },
   },
