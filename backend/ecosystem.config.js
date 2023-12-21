ffe58e85c794a409c3bfa6e48bd7a64db83ce4c4
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-12-21 23:11:18 +0300

    исправлен ecosystem.config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend && scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 restart ecosystem.config.js`,
     },
   },
 };
