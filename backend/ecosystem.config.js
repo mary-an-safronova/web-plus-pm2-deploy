f5d8dffee93b6fddbd0f048a48d4bf7d653cc0d4
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-12-21 23:33:10 +0300

    исправлен ecosystem.config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend && scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': 'cd ~/home/mary_safronova/source/backend && npm i && npm run build && pm2 restart ecosystem.config.js',
+      'post-deploy': 'cd ~/source/backend && npm i && npm run build && pm2 restart ecosystem.config.js',
     },
   },
 };
--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/frontend && scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/frontend`,
-      'post-deploy': `cd ~/home/mary_safronova/source/frontend && npm i && npm run build`,
+      'post-deploy': `cd ~/source/frontend && npm i && npm run build`,
     },
   },
 }
